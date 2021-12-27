56a11e418423f4ea5b754bd64dae75ec153b2202
Johannes <92708671+Johannes2803@users.noreply.github.com>
2021-12-27 21:38:23 +0700

    Update sc.js

--- plugins/sc.js
+++ plugins/sc.js
@@ -1,6 +1,6 @@
 let handler = async m => m.reply(`
 Bot Ini Mengunakan Sc : 
-https://github.com/BochilGaming/games-wabot
+https://github.com/Johannes2803
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['sc']
 handler.tags = ['info']
