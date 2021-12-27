13c3cb525b1f734b75524de43f00f274bea34b5d
Johannes <92708671+Johannes2803@users.noreply.github.com>
2021-12-27 09:42:53 +0700

    Update _autolevelup.js

--- plugins/_autolevelup.js
+++ plugins/_autolevelup.js
@@ -1,23 +1,22 @@
 let handler = m => m
+
 let levelling = require('../lib/levelling')
 handler.before = m => {
-    let name = conn.getName(m.sender)
-    let user = global.DATABASE._data.users[m.sender]
-    if (!user.autolevelup) return
-    if (m.sender === global.conn.user.jid) return
-    let before = user.level * 1
-    while (levelling.canLevelUp(user.level, user.exp, global.multiplier)) user.level++
-    if (before !== user.level) {
-        let str =  `
+	let name = conn.getName(m.sender)
+	let user = global.DATABASE._data.users[m.sender]
+	if (!user.autolevelup) return !0
+	let before = user.level * 1
+	while (levelling.canLevelUp(user.level, user.exp, global.multiplier)) user.level++
+        
+	if (before !== user.level) {
+            m.reply(`
 「 SELAMAT 」
 ➸ Name : ${name}
 ➸ XP : ${user.exp}
 ➸ Level :*${before}* -> *${user.level}* 
 Congratulations 🎉
- `.trim() 
-        conn.sendMessage(m.chat, str, m,)
-    }
-    return true
+	`.trim())
+        }
 }
- 
+
 module.exports = handler
