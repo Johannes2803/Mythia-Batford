34c991a688c5352ff94101da02d742dcfb89d44d
Johannes <92708671+Johannes2803@users.noreply.github.com>
2021-12-27 09:12:53 +0700

    Update yts.js

--- plugins/yts.js
+++ plugins/yts.js
@@ -5,10 +5,11 @@ let handler = async (m, { text }) => {
   let teks = results.all.map(v => {
     switch (v.type) {
       case 'video': return `
-*${v.title}* (${v.url})
-Duration: ${v.timestamp}
-Uploaded ${v.ago}
-${v.views} views
+» Judul : *${v.title}* 
+» Link : ${v.url}
+» Duration: ${v.timestamp}
+» Uploaded : ${v.ago}
+» Views : ${v.views} views
       `.trim()
       case 'channel': return `
 *${v.name}* (${v.url})
@@ -16,7 +17,7 @@ _${v.subCountLabel} (${v.subCount}) Subscriber_
 ${v.videoCount} video
 `.trim()
     }
-  }).filter(v => v).join('\n========================\n')
+  }).filter(v => v).join('\n\n────────────────\n\n')
   m.reply(teks)
 }
 handler.help = ['', 'earch'].map(v => 'yts' + v + ' <pencarian>')
