64a006e05772259a56c26bf63abddc86f8118d08
Johannes <92708671+Johannes2803@users.noreply.github.com>
2021-12-27 12:48:12 +0700

    Update waifu.js

--- plugins/waifu.js
+++ plugins/waifu.js
@@ -1,14 +1,15 @@
 let fetch = require('node-fetch')
-
-let handler = async (m, { conn, usedPrefix, command }) => {
-    let res = await fetch(API('amel', '/waifu', {}, 'apikey'))
-    if (!res.ok) throw eror
-    let json = await res.json()
-    if (!json.status) throw json
-    conn.sendButtonImg(m.chat, json.result, 'Nieh banh waifunya', wm, 'waifu again', `${usedPrefix + command}`, m)
+let handler = async (m, { conn }) => {
+  let res = await fetch('https://api.waifu.pics/sfw/waifu')
+  if (!res.ok) throw 'Error Website sedang down'
+  let json = await res.json()
+  if (!json.url) throw 'Error!'
+  conn.sendFile(m.chat, json.url, '', 'istri gweh', m, 0, { thumbnail: Buffer.alloc(0) })
 }
 handler.help = ['waifu']
 handler.tags = ['anime']
 handler.command = /^(waifu)$/i
+
+handler.limit = true
 
 module.exports = handler
