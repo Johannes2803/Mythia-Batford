bc5e56c6329af4f5bd421b9b88ee987a1fc1d0bf
Johannes <92708671+Johannes2803@users.noreply.github.com>
2021-12-27 23:26:14 +0700

    Update waifu.js

--- plugins/waifu.js
+++ plugins/waifu.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn }) => {
   if (!res.ok) throw 'Error Website sedang down'
   let json = await res.json()
   if (!json.url) throw 'Error!'
-  conn.sendFile(m.chat, json.url, '', 'istri gweh', m, 0, { thumbnail: Buffer.alloc(0) })
+  conn.sendFile(m.chat, json.url, '', 'istri gweh', m)
 }
 handler.help = ['waifu']
 handler.tags = ['anime']
