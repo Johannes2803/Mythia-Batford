2fc5b937e8c6c36e08a88d5cb764ef80bf2247a7
Johannes <92708671+Johannes2803@users.noreply.github.com>
2021-12-28 15:33:09 +0700

    Fix

--- plugins/tagall.js
+++ plugins/tagall.js
@@ -1,17 +1,12 @@
-let { MessageType } = require('@adiwajshing/baileys')
-let handler = async(m, { conn, text, participants }) => {
-  let teks = `══✪〘 *👥 Mention All* 〙✪══\n\n➲ *Message : ${text ? text : 'Nothing'}*\n\n`
-		      	for (let mem of participants) {
-		            teks += `࿃➡️ @${mem.id.split('@')[0]}\n`
-				}
-                teks += `\n⋙ *Admin Group* ⋘`
-                conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
+let handler = async (m, { conn, text, participants }) => {
+  let users = participants.map(u => u.jid)
+  m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
+    contextInfo: { mentionedJid: users }
+  })
 }
-handler.help = ['tagall <pesan>']
-handler.tags = ['group']
-handler.command = /^(tagall)$/i
+handler.command = ['tagall']
 
-handler.group = true
 handler.admin = true
+handler.group = true
 
 module.exports = handler
