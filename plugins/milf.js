45704746e29a4d9fc17269a079b2708b99426ce1
Johannes <92708671+Johannes2803@users.noreply.github.com>
2021-12-29 01:59:24 +0700

    Fix eror

--- plugins/milf.js
+++ plugins/milf.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn }) => {
   m.reply('Loading...')
-  let res = `https://velgrynd.herokuapp.com/api/image/milf`
-  conn.sendFile(m.chat, res, 'milf.jpg', 'huu suka ama tante tante', m)
+  let res = `https://api.ichikaa.xyz/api/randomimage/milf?apikey=79lJ9HEA`
+  conn.sendFile(m.chat, res, 'milf.jpg', 'huu suka ama milf', m)
 }
 handler.help = ['milf']
 handler.tags = ['anime']
