72ebeb1e91fc2a06fbe145e59c6abf5d097e2a6d
Johannes <92708671+Johannes2803@users.noreply.github.com>
2022-01-11 21:47:03 +0700

    Fix eror

--- plugins/ig.js
+++ plugins/ig.js
@@ -1,33 +1,14 @@
-let fetch = require('node-fetch')
+let scraper = require('@bochilteam/scraper')
 
-let handler = async (m, { conn, command, args, usedPrefix }) => {
-    if (!args[0]) throw `uhm.. urlnya mana?\n\npenggunaan:\n${usedPrefix + command} url\ncontoh:\n${usedPrefix + command} https://www.instagram.com/p/COaLQGnJFUn/`
-    if (!/https?:\/\/(www\.)?instagram\.com\/(.*\/)?(p|reel|tv|stories)/i.test(args[0])) throw `url salah! yang bisa post, reels, tv and story`
-    if (/https?:\/\/(www\.)?instagram\.com\/(.*\/)?(stories)/i.test(args[0])) {
-        let res = await fetch(API('amel', '/igs', { url: args[0] }, 'apikey'))
-        if (!res.ok) throw eror
-        let json = await res.json()
-        if (!json.status) throw json
-        await m.reply(wait)
-        for (let { url, type } of json.data) {
-            await conn.sendFile(m.chat, url, 'igs' + (type == 'jpg' ? '.jpg' : '.mp4'), '', m)
-        }
-    }
-    if (/https?:\/\/(www\.)?instagram\.com\/(.*\/)?(p|reel|tv)/i.test(args[0])) {
-        let res = await fetch(API('amel', '/ig', { url: args[0] }, 'apikey'))
-        if (!res.ok) throw eror
-        let json = await res.json()
-        if (!json.status) throw json
-        await m.reply(wait)
-        for (let { url, fileType } of json.data) {
-            await conn.sendFile(m.chat, url, 'ig' + (fileType == 'image' ? '.jpg' : '.mp4'), '', m)
-        }
-    }
+let handler = async (m, { conn, args }) => {
+  if (!args[0]) throw 'Uhm...url nya mana?'
+  await m.reply('Loading...')
+  let res = await scraper.instagramdl(args[0])
+  for (let i = 0; i < res.length; i++) await conn.sendFile(m.chat, res[i].url, '', '', m)
 }
-handler.help = ['instagram'].map(v => v + ' <url>')
-handler.tags = ['download']
-handler.command = /^(instagram|ig)$/i
-
-handler.limit = 1
+handler.help = ['ig'].map(v => v + ' <url>')
+handler.tags = ['downloader']
+handler.limit = true
+handler.command = /^(ig(dl)?)$/i
 
 module.exports = handler
