d91bde48df88e84fe9fbdad6579a418bb15260a3
Johannes <92708671+Johannes2803@users.noreply.github.com>
2022-01-14 21:12:28 +0700

    Sedikit ubah

--- plugins/pinterestvideo.js
+++ plugins/pinterestvideo.js
@@ -2,14 +2,14 @@ const { pin } = require('../lib/scrape')
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 
-    if (!args[0]) throw `*Perintah ini untuk mengunduh video dari pinterest dengan link*\n\ncontoh:\n${usedPrefix + command} https://id.pinterest.com/pin/267893877823775677/`
-    if (!args[0].match(/https:\/\/.*pinterest.com\/pin|pin.it/gi)) throw `*Link salah! Perintah ini untuk mengunduh video dari pinterest dengan link*\n\ncontoh:\n${usedPrefix + command} https://id.pinterest.com/pin/267893877823775677/`
+    if (!args[0]) throw `*Perintah ini untuk mengunduh video dari pinterest dengan link*\n\ncontoh:\n${usedPrefix + command} https://id.pinterest.com/pin/27162403992537372/`
+    if (!args[0].match(/https:\/\/.*pinterest.com\/pin|pin.it/gi)) throw `*Link salah! Perintah ini untuk mengunduh video dari pinterest dengan link*\n\ncontoh:\n${usedPrefix + command} https://id.pinterest.com/pin/27162403992537372/`
 
     pin(args[0]).then(async res => {
         let pin = JSON.stringify(res)
         let json = JSON.parse(pin)
         if (!json.status) throw `Tidak dapat diunduh`
-        await conn.sendVideo(m.chat, json.data.url, `*YUTABOTZ*`, m)
+        await conn.sendVideo(m.chat, json.data.url, `*Mythia Batford*`, m)
     })
 
 }
