44d022903bdde7199ff02e6e05d2b19641b6c337
Johannes <92708671+Johannes2803@users.noreply.github.com>
2022-01-15 22:24:09 +0700

    Ganti api

--- plugins/quotesanime.js
+++ plugins/quotesanime.js
@@ -1,15 +1,19 @@
 let axios = require("axios");
 let fetch = require('node-fetch')
 let handler = async(m, { conn, text }) => {
-let ye = await (await fetch('https://i.pinimg.com/originals/94/e7/21/94e721efd82d3aba7e7da4a0bde1b036.jpg')).buffer()
-	axios.get(`https://api.lolhuman.xyz/api/random/quotesnime?apikey=HIRO`).then ((res) => {
+try {
+imeg = await conn.getProfilePicture(conn.user.jid)
+} catch {
+imeg = 'https://i.pinimg.com/originals/94/e7/21/94e721efd82d3aba7e7da4a0bde1b036.jpg'
+}
+	axios.get(`https://api.lolhuman.xyz/api/random/quotesnime?apikey=1221a9c62630c6207ebb4761`).then ((res) => {
 		
 prep = conn.prepareMessageFromContent(m.chat, { orderMessage: { 
 itemCount: 99999999, status: 1,
 message: `_Quote: ${res.data.result.quote}_\n\n*• Character: ${res.data.result.character}*\n*• Anime: ${res.data.result.anime} ${res.data.result.episode}*`,
 orderTitle: 'B',
 sellerJid: '62895603352610@s.whatsapp.net',
-thumbnail: ye
+thumbnail: await (await require('node-fetch')(imeg)).buffer(),
 }}, {contextInfo: null, quoted: m})
 conn.relayWAMessage(prep)
   })
