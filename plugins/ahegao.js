873663833f61e15697995e54c7ab2db39c4ab533
Johannes <92708671+Johannes2803@users.noreply.github.com>
2022-01-15 22:30:52 +0700

    Ganti api

--- plugins/ahegao.js
+++ plugins/ahegao.js
@@ -3,8 +3,8 @@ let handler = async (m, { conn, args }) => {
    response = args.join(' ')
   if (!args) throw 'Masukkan Parameter'
   m.reply('Sedang Diproses...')
-  let res = `https://api.lolhuman.xyz/api/random/nsfw/ahegao?apikey=HIRO`
-  conn.sendFile(m.chat, res, 'ahegao.jpg', `wangy wangy wangy`, m, false)
+  let res = `https://api.lolhuman.xyz/api/random/nsfw/ahegao?apikey=1221a9c62630c6207ebb4761`
+  conn.sendFile(m.chat, res, 'ahegao.jpg', `Wangy Wangy wangy`, m, false)
 }
 handler.help = ['ahegao'].map(v => v + ' ')
 handler.tags = ['dewasa']
