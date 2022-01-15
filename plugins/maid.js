cbb26e16811e1087578c747712732682a33a2bb0
Johannes <92708671+Johannes2803@users.noreply.github.com>
2022-01-15 15:02:45 +0700

    Ganti api

--- plugins/maid.js
+++ plugins/maid.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn }) => {
   m.reply('Loading...')
-  let res = `https://api.ichikaa.xyz/api/randomimage?q=maid&apikey=AAgXXQeo`
+  let res = `https://velgrynd.herokuapp.com/api/randomimage?q=maid&apikey=IchikaKey`
   conn.sendFile(m.chat, res, 'maid.jpg', 'huu suka ama maid', m)
 }
 handler.help = ['maid']
