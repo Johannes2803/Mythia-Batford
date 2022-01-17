4f6f28ec387652e930334daff5f369daf9d9eb6a
Johannes <92708671+Johannes2803@users.noreply.github.com>
2022-01-18 03:50:56 +0700

    Update simulasi.js

--- plugins/simulasi.js
+++ plugins/simulasi.js
@@ -1,33 +1,35 @@
 let handler = async (m, { conn, args: [event], text }) => {
-    if (!event) throw `List Event: welcome, bye, delete, promote, demote`
     let mentions = text.replace(event, '').trimStart()
     let who = mentions ? conn.parseMention(mentions) : []
     let participants = who.length ? who : [m.sender]
     let action = false
-    m.reply(`Simulating ${event}...`)
     switch (event.toLowerCase()) {
         case 'add':
         case 'invite':
         case 'welcome':
+            if (!global.DATABASE.data.chats[m.chat].welcome) return await conn.sendButton(m.chat, 'welcome belum diaktikan!', `${conn.getName(conn.user.jid)}`, 'aktifkan', '.on welcome', m)
             action = 'add'
             break
         case 'bye':
         case 'kick':
         case 'leave':
         case 'remove':
+            if (!global.DATABASE.data.chats[m.chat].welcome) return await conn.sendButton(m.chat, 'welcome belum diaktikan!', `${conn.getName(conn.user.jid)}`, 'aktifkan', '.on welcome', m)
             action = 'remove'
             break
         case 'promote':
+            if (!global.DATABASE.data.chats[m.chat].welcome) return await conn.sendButton(m.chat, 'detect belum diaktikan!', `${conn.getName(conn.user.jid)}`, 'aktifkan', '.on detect', m)
             action = 'promote'
             break
         case 'demote':
+            if (!global.DATABASE.data.chats[m.chat].welcome) return await conn.sendButton(m.chat, 'detect belum diaktikan!', `${conn.getName(conn.user.jid)}`, 'aktifkan', '.on detect', m)
             action = 'demote'
             break
         case 'delete':
+            if (global.DATABASE.data.chats[m.chat].delete) return await conn.sendButton(m.chat, 'delete is active!', `${conn.getName(conn.user.jid)}`, 'matikan', '.on antidelete', m)
             deleted = m
             break
-        default:
-            throw `List Event: welcome, bye, delete, promote, demote`
+        default: throw `daftar event: add, bye, promote, demote, delete`
     }
     if (action) return conn.onParticipantsUpdate({
         jid: m.chat,
@@ -36,8 +38,8 @@ let handler = async (m, { conn, args: [event], text }) => {
     })
     return conn.onDelete(m)
 }
-handler.help = ['simulate <event> [@mention]']
-handler.tags = ['owner']
+handler.help = ['simulasi <event> [@tag]']
+handler.tags = ['group']
+handler.command = /^simula(si|te)$/i
 
-handler.command = /^simulate$/i
 module.exports = handler
