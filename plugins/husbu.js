eed1661027bbd6b0cc960f740ddd45fb62a1e1f0
Johannes <92708671+Johannes2803@users.noreply.github.com>
2022-01-18 03:39:09 +0700

    Update husbu.js

--- plugins/husbu.js
+++ plugins/husbu.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
 let res = await axios.get(API('amel', '/husbu', {}, 'apikey'))
 
 let json = res.data
-    conn.sendButtonImg(m.chat, json.result.image, json.result.teks, 'tes', 'husbu again', `${usedPrefix + command}`, m)
+    conn.sendButtonImg(m.chat, json.result.image, json.result.teks, 'MYTHIA×YUTA', 'husbu again', `${usedPrefix + command}`, m)
 }
 handler.help = ['anime']
 handler.tags = ['fun']
