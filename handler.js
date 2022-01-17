e25349faecb53e8f18f4dbd9f4ebe4183773e642
Johannes <92708671+Johannes2803@users.noreply.github.com>
2022-01-17 17:17:03 +0700

    Fix welcome

--- handler.js
+++ handler.js
@@ -569,14 +569,14 @@ ${(global.linkGC).map((v, i) => '*Group ' + (i + 1) + '*\n' + v).join`\n\n`}
             } finally {
               text = (action === 'add' ? (chat.sWelcome || this.welcome || conn.welcome || 'selamat datang, @user!').replace('@subject', this.getName(jid)).replace('@desc', groupMetadata.desc ? String.fromCharCode(8206).repeat(4001) + groupMetadata.desc : '') :
                 (chat.sBye || this.bye || conn.bye || 'sampai jumpa, @user!')).replace(/@user/g, '@' + user.split('@')[0])
-              let wel = API('Velgrynd', 'api/welcome', {
+              let wel = API('Velgrynd', '/api/welcome', {
                 name: this.getName(user),
                 picurl: pp,
                 bgurl: 'https://telegra.ph/file/4b90043328ec4825c0e71.jpg',
                 mem: groupMetadata.participants.length,
                 gcname: this.getName(jid)
               }, 'apikey')
-              let lea = API('Velgrynd', '/goodbye', {
+              let lea = API('Velgrynd', '/api/goodbye', {
                 name: this.getName(user),
                 picurl: pp,
                 bgurl: 'https://telegra.ph/file/4b90043328ec4825c0e71.jpg',
