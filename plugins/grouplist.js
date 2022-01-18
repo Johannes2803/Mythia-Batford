e3acd1d4d0ed5a7db2f706b5c0265d636ac36411
Johannes <92708671+Johannes2803@users.noreply.github.com>
2022-01-18 18:00:19 +0700

    Just Copas :v

--- plugins/grouplist.js
+++ plugins/grouplist.js
@@ -1,12 +1,34 @@
 let handler = async (m, { conn }) => {
-  let group = conn.chats.all().filter(v => v.jid.endsWith('g.us'))
-  let txt = ''
-  for (let v of group) txt += `${await conn.getName(v.jid)}\n${v.jid} [${v.read_only ? 'Left' : 'Joined'}]\n\n`
-  m.reply( 'List Groups:\n' + txt.trim())
+  let txt = conn.chats.all().filter(v => v.jid.endsWith('g.us')).map(v => `
+${conn.getName(v.jid)}\n${v.jid} [${v.read_only ? 'keluar' : 'masuk'}]\n${global.DATABASE.data.chats[v.jid] == undefined ? global.DATABASE.data.chats[v.jid] = {
+      isBanned: false,
+      welcome: true,
+      detect: true,
+      sWelcome: '',
+      sBye: '',
+      sPromote: '',
+      sDemote: '',
+      antiLink: true,
+      autoread: false,
+      broadcast: true,
+      clear: false,
+      clearTime: (new Date() * 1) + 3600000 * 1,
+      delete: true,
+      desc: true,
+      download: true,
+      getmsg: false,
+      groupTime: 0,
+      stiker: false,
+      viewonce: true,
+    } : global.DATABASE.data.chats[v.jid].groupTime ? conn.msToDate(global.DATABASE.data.chats[v.jid].groupTime - new Date() * 1) : 'belum diatur'}
+${global.DATABASE.data.chats[v.jid].clear ? '✅' : '❌'} otomatis hapus chat
+${global.DATABASE.data.chats[v.jid].autoread ? '✅' : '❌'} otomatis membaca pesan
+${global.DATABASE.data.chats[v.jid].broadcast ? '✅' : '❌'} pesan siaran
+    `.trim()).join`\n\n`
+  m.reply(txt)
 }
 handler.help = ['groups', 'grouplist']
 handler.tags = ['info']
-handler.command = /^(gro?up(s|list)|listgro?ups?)$/i
+handler.command = /^(group(s|list))$/i
 
 module.exports = handler
-
