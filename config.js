686727a6abda938334214610a988fc63cd2625ef
Johannes <92708671+Johannes2803@users.noreply.github.com>
2022-03-29 08:22:43 +0700

    Sedikit perubahan

--- config.js
+++ config.js
@@ -1,11 +1,14 @@
 let fs = require('fs')
-let image1 = fs.readFileSync('./logo4.jpg')
-let image2 = fs.readFileSync('./logo2.jpg')
-let image3 = fs.readFileSync('./logo5.jpg')
+let image1 = fs.readFileSync('./media/logo4.jpg')
+let image2 = fs.readFileSync('./media/logo2.jpg')
+let image3 = fs.readFileSync('./media/logo5.jpg')
+let image4 = fs.readFileSync('./media/logo3.jpg')
+let image5 = fs.readFileSync('./media/logo.jpg')
 global.DeveloperMode = 'false' //true Or false
-global.linkGC = ['https://chat.whatsapp.com/Ix8SHYHTYdB98ERHBUI2NA']
-global.owner = ['3197010258237', '17608914335', '6285892009320', '6283829756773', '6285755950256'] // Letakan nomor kamu disini
-global.mods = [] // Moderator?
+global.linkGC = ['https://chat.whatsapp.com/DYbwxUvMEzTEsOuYQnBDm2']
+global.owner = ['6289637133848'] // Letakan nomor kamu disini
+global.kontak = ['6289637133848'] //entah lahðŸ—¿
+global.mods = ['17608914335', '6283829756773', '6285755950256', '6281222314374', '6285814839139'] // Moderator?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // Pengguna premium tidak memerlukan limit
 global.APIs = { // API Prefix
   // nama: 'https://website'
@@ -32,26 +35,34 @@ global.APIKeys = { // APIKey nya disini
   'https://neoxr-api.herokuapp.com': 'yntkts',
   'https://rikka-api.herokuapp.com': 'beta',
   'https://pencarikode.xyz': 'pais',
-  'https://melcanz.net': 'YOU_APIKEY',
-  'https://api.lolhuman.xyz': 'YOU_APIKEY',
-  'https://api.xteam.xyz': 'YOU_APIKEY',
+  'https://melcanz.net': 'DUHqfyQF',
+  'https://api.lolhuman.xyz': 'Deffbotz',
+  'https://api.xteam.xyz': 'Dawnfrostkey',
   'https://leyscoders-api.herokuapp.com': 'MIMINGANZ', 
   'https://zahirr-web.herokuapp.com': 'zahirgans',
   'https://api.zeks.xyz': 'ASJJMOFsjrVE8eZq1jgtGL4rGVE',
   'https://velgrynd.herokuapp.com': '79lJ9HEA'
 }
 
-// Sticker WM
+//*****************STICKER WM*********************
 global.packname = 'Sticker'
-global.author = 'By Mythia-batford'
+global.author = 'By Wabot'
+//*****************BAGIAN IMAGE*********************
 global.image = image1
 global.image2 = image2
 global.image3 = image3
-
+global.image4 = image4
+global.image5 = image5
+//*****************BAGIAN GLOBAL CHAT*********************
+global.wm = '©Mythia Batford'
+global.rpg = 'Fitur Rpg Tidak Aktif\nKetik *!on* *rpg* untuk menggunakan fitur ini!\nKalo Mau main Disini aja\nhttps://chat.whatsapp.com/DYbwxUvMEzTEsOuYQnBDm2'
+global.nsfw = 'Fitur NSFW Tidak Aktif\nKetik *!on* *nsfw* untuk menggunakan fitur ini!'
 global.eror = 'Error 404'
 global.wait = '_tunggu_'
-
+//*****************BAGIAN LEVEL*********************
 global.multiplier = 69 // Semakin tinggi, semakin sulit naik level
+//*****************PEMBATAS*********************
+// JANGAN DI GANTI NTAR KLO GABISA JAN TANYA GW
 
 let chalk = require('chalk')
 let file = require.resolve(__filename)
